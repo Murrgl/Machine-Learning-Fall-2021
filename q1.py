60b443df570b40f8d27b192209f63213d4b6e61d
Murrgl <thaigreentea@gmail.com>
2021-10-03 17:58:17 -0400

    Add files via upload

--- q1.py
+++ q1.py
@@ -35,32 +35,32 @@ class GraphCreation(object):
         return featureMFCC10, featureMFCC17, hylaClass, hypsiboasClass
 #   Draws the 2 scatter plots.
     def drawScatterPlots(self, file_name, name):
-        f1, f2, class1, class2 = self.fileManipulation(file_name)
+        featureMFCC10, featureMFCC17, hylaClass, hypsiboasClass = self.fileManipulation(file_name)
 
-        plt.scatter(class1[:,0], class1[:,1], c='magenta',
+        plt.scatter(hylaClass[:,0], hylaClass[:,1], c='magenta',
                     label="HylaMinuta")
-        plt.scatter(class2[:,0], class2[:,1], c='orange',
+        plt.scatter(hypsiboasClass[:,0], hypsiboasClass[:,1], c='orange',
                     label="HypsiboasCinerascens")
         plt.title('Scatter plot for both classes in ' + name)
         plt.legend(loc='upper right')
-        plt.xlabel(f1 + ' values')
-        plt.ylabel(f2 + ' values')
+        plt.xlabel(featureMFCC10 + ' values')
+        plt.ylabel(featureMFCC17 + ' values')
         plt.show()
 
     #  Here I am drawing 2 histograms for each class [Hyla and Hypsi] (1 per feature) for each frog in each file.
     def drawHistograms(self, file_name, name):
-        f1, f2, class1, class2 = self.fileManipulation(file_name)
+        featureMFCC10, featureMFCC17, hylaClass, hypsiboasClass = self.fileManipulation(file_name)
 
-        plt.hist((class1[:,0],class1[:,1]), color=('magenta','orange'), bins=20,
-        label=(f1, f2))
+        plt.hist((hylaClass[:,0],hylaClass[:,1]), color=('magenta','orange'), bins=20,
+        label=(featureMFCC10, featureMFCC17))
         plt.title('Histograms of MFCCs for Hyla minuta from ' + name)
         plt.xlabel('Number of Data Points')
         plt.ylabel('Values of Features')
         plt.legend(loc='upper right')
         plt.show()
 
-        plt.hist((class2[:, 0], class2[:, 1]), color=('cyan', 'red'),
-                 bins=20, label=(f1, f2))
+        plt.hist((hypsiboasClass[:, 0], hypsiboasClass[:, 1]), color=('cyan', 'red'),
+                 bins=20, label=(featureMFCC10, featureMFCC17))
         plt.title('Histograms of MFCCs for Hypsiboas cinerascens From '
                   '' + name)
         plt.xlabel('Number of Data Points')
@@ -70,22 +70,22 @@ class GraphCreation(object):
 
     #  Here I am drawing 2 line graphs for each class [Hyla and Hypsi] (1 per feature) for each frog in each file.
     def drawLineGraphs(self, file_name, name):
-        f1, f2, class1, class2 = self.fileManipulation(file_name)
+        featureMFCC10, featureMFCC17, hylaClass, hypsiboasClass = self.fileManipulation(file_name)
 
-        t1 = np.sort(class1[:,0])
-        t2 = np.sort(class1[:,1])
-        plt.plot(np.arange(t1.shape[0]), t1, color='magenta', label=f1)
-        plt.plot(np.arange(t2.shape[0]), t2, color='yellow', label=f2)
+        t1 = np.sort(hylaClass[:,0])
+        t2 = np.sort(hylaClass[:,1])
+        plt.plot(np.arange(t1.shape[0]), t1, color='magenta', label=featureMFCC10)
+        plt.plot(np.arange(t2.shape[0]), t2, color='yellow', label=featureMFCC17)
         plt.title('Line graph of MFCCs for Hyla minuta from ' + name)
         plt.xlabel('Number of Data Points')
         plt.ylabel('Values of Features')
         plt.legend(loc='upper right')
         plt.show()
 
-        t1 = np.sort(class2[:, 0])
-        t2 = np.sort(class2[:, 1])
-        plt.plot(np.arange(t1.shape[0]), t1, color='magenta', label=f1)
-        plt.plot(np.arange(t2.shape[0]), t2, color='yellow', label=f2)
+        t1 = np.sort(hypsiboasClass[:, 0])
+        t2 = np.sort(hypsiboasClass[:, 1])
+        plt.plot(np.arange(t1.shape[0]), t1, color='magenta', label=featureMFCC10)
+        plt.plot(np.arange(t2.shape[0]), t2, color='yellow', label=featureMFCC17)
         plt.title('Line Graph of MFCCs for Hypsiboas cinerascens from ' + name)
         plt.xlabel('Number of Data Points')
         plt.ylabel('Values of Features')
@@ -94,15 +94,15 @@ class GraphCreation(object):
 
     #  Here I am drawing 1 box plot.
     def drawBoxPlot(self, file_name, name):
-        f1, f2, class1, class2 = self.fileManipulation(file_name)
+        featureMFCC10, featureMFCC17, hylaClass, hypsiboasClass = self.fileManipulation(file_name)
 
-        plt.boxplot(class1[:,:2])
+        plt.boxplot(hylaClass[:,:2])
         plt.title('Boxplot for Hyla minuta from ' + name)
         plt.xlabel('Features')
         plt.ylabel('Values')
         plt.show()
 
-        plt.boxplot(class2[:, :2])
+        plt.boxplot(hypsiboasClass[:, :2])
 
 
         plt.title('Box Plot for Hypsiboas cinerascens from ' + name)
@@ -112,14 +112,14 @@ class GraphCreation(object):
 
     #  Here I am drawing 1 box plot.
     def drawErrorBar(self, file_name, name):
-        f1, f2, class1, class2 = self.fileManipulation(file_name)
+        featureMFCC10, featureMFCC17, hylaClass, hypsiboasClass = self.fileManipulation(file_name)
 
         x_pos = np.arange(4)
-        CTEs = [np.mean(class1[:,0]), np.mean(class1[:,1]),
-                np.mean(class2[:,0]), np.mean(class2[:,1])]
-        error = [np.std(class1[:,0]), np.std(class1[:,1]),
-                np.std(class2[:,0]), np.std(class2[:,1])]
-        plt.bar(x_pos, CTEs, yerr=error, align='center', alpha=0.5,
+        CTEs = [np.mean(hylaClass[:,0]), np.mean(hylaClass[:,1]),
+                np.mean(hypsiboasClass[:,0]), np.mean(hypsiboasClass[:,1])]
+        error = [np.std(hylaClass[:,0]), np.std(hylaClass[:,1]),
+                np.std(hypsiboasClass[:,0]), np.std(hypsiboasClass[:,1])]
+        plt.bar(x_pos, CTEs, yerr=error, align='center', alpha=0.6,
                 ecolor='black', capsize=10)
         plt.tight_layout()
         plt.title('Bar Graph With Error Bars for' + name)
@@ -127,24 +127,25 @@ class GraphCreation(object):
         plt.ylabel('Values')
         plt.show()
 
+    #1b.
     def calculateStatistics(self, file_name, name):
-        f1 = list(file_name)[0]
-        f2 = list(file_name)[1]
+        featureMFCC10 = list(file_name)[0]
+        featureMFCC17 = list(file_name)[1]
         data = file_name.to_numpy()
 
-        f1_mean = np.mean(data[:,0])
-        f2_mean = np.mean(data[:,1])
-        f1_std = np.std(data[:,0])
-        f2_std = np.std(data[:,1])
+        featureMFCC10_mean = np.mean(data[:,0])
+        featureMFCC17_mean = np.mean(data[:,1])
+        hylaClass_STD = np.std(data[:,0])
+        hypsiboasClass_STD = np.std(data[:,1])
 
         temp = data[:,:2].astype(float)
-        cov_matrix = np.cov(temp[:,:2].T)
+        covarianceMatrix = np.cov(temp[:,:2].T)
         print('Statistics for',name)
-        print('Expected Value for', f1, ': ', f1_mean)
-        print('Expected Value for', f2, ': ', f2_mean)
-        print('Covariance Matrix:\n', cov_matrix)
-        print('STD for', f1, ': ', f1_std)
-        print('STD for', f2, ': ', f2_std)
+        print('Expected Value for', featureMFCC10, ': ', featureMFCC10_mean)
+        print('Expected Value for', featureMFCC17, ': ', featureMFCC17_mean)
+        print('Covariance Matrix:\n', covarianceMatrix)
+        print('STD for', featureMFCC10, ': ', hylaClass_STD)
+        print('STD for', featureMFCC17, ': ', hypsiboasClass_STD)
         print()
 
 
@@ -177,7 +178,7 @@ class GraphCreation(object):
         self.calculateStatistics(self.frogsCSV, 'Frogs.CSV')
         self.calculateStatistics(self.frogsSampleCSV, 'Frogs-subsample.CSV')
 
-
+#   Creates an object from the GraphCreation class so we can generate the graphs.
 if __name__ == '__main__':
     localGraph = GraphCreation()
     localGraph.makeGraphs()
