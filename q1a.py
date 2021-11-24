588ee0b4e298d696f02cb971a3e3d495d84b3d2f
Murrgl <thaigreentea@gmail.com>
2021-11-24 17:22:52 -0500

    Add files via upload

--- q1a.py
+++ q1a.py
@@ -4,102 +4,93 @@
 # Homework 3: Multiclass Classification
 # Problem: q1a.py
 
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import os
 
-
-plt.rcParams['figure.figsize'] = (10.0, 8.0)  # set default size of plots
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
-
-
-#This numerical gradient algorithm returns an approximation of nabla.
-#It follows the 8 steps listed on page 3 of the homework.
-def gradientUpdateRule(X, y, theta, reg):
-    epsilon = 1e-5
-    nabla_n = []
-    # NOTE: you do not have to use any of the code here in your implementation...
-    for i in range(len(theta)):
-        param = theta[i]
-        param_grad = np.zeros(param.shape)
-        it = np.nditer(param, flags=['multi_index'], op_flags=['readwrite'])
-        while not it.finished:
+# This numerical gradient algorithm calculates an approximation of nabla.
+# It follows the 8 steps listed on page 3 of the homework.
+def numericalGradientAlgorithm(inputData, outputData, theta, regularizationNum):
+    epsilonNum = 0.0001 # The HW suggests a value of 0.001 or 0.0001.
+    nablaArray = []
+    for index in range(len(theta)):
+        parameter = theta[index]
+        gradient = np.zeros(parameter.shape)
+        iterator = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
+        while not iterator.finished:
             # parameters at (x+epsilon) and (x-epsilon)
-            theta_plus_eps = theta
-            theta_minus_eps = theta
-            ix = it.multi_index
-            # Evaluating function at x+epsilon i.e f(x+epsilon)
-            theta_plus_eps[i][ix] = param[ix] + epsilon
-            f_x_plus_eps = computeCost(X, y, theta_plus_eps, reg)
-            # Reseting theta
-            theta[i][ix] = param[ix] - epsilon
-            # Evaluating function at x i.e f(x-epsilon)
-            theta_minus_eps[i][ix] = param[ix] - epsilon
-            f_x_minus_eps = computeCost(X, y, theta_minus_eps, reg)
-            # Reseting theta
-            theta[i][ix] = param[ix] + epsilon
-            # gradient at x
-            param_grad[ix] = (f_x_plus_eps - f_x_minus_eps) / (2 * epsilon)
-            # Iterating over all dimensions
-            it.iternext()
-        nabla_n.append(param_grad)
-    return tuple(nabla_n)
+            thetaPlusEpsilon = theta
+            thetaMinusEpsilon = theta
+            indexMulti = iterator.multi_index
+            # 1. Add ϵ to Θj, put Θj back into Θ, giving us (Θ + ϵ) at j
+            thetaPlusEpsilon[index][indexMulti] = parameter[indexMulti] + epsilonNum
+            # 2. Given perturbed Θj (all the rest of the values in Θ are fixed), calculate J (Θ + ϵ)
+            perturbedThetaPlusEpsilon = calculateCost(inputData, outputData, thetaPlusEpsilon, regularizationNum)
+            # 3. Resetting theta to its original state.
+            theta[index][indexMulti] = parameter[indexMulti] - epsilonNum
+            # 4. Subtract ϵ from Θj
+            thetaMinusEpsilon[index][indexMulti] = parameter[indexMulti] - epsilonNum
+            # 5. Given perturbed Θj, calculate J (Θ − ϵ)
+            perturbedThetaMinusEpsilon = calculateCost(inputData, outputData, thetaMinusEpsilon, regularizationNum)
+            # 6. Estimate the scalar derivative using Equation 5
+            # 7. Reset b to its original state, returning ∼ ∇Θ
+            theta[index][indexMulti] = parameter[indexMulti] + epsilonNum
+            gradient[indexMulti] = (perturbedThetaPlusEpsilon - perturbedThetaMinusEpsilon) / (2 * epsilonNum)
+            # 8. Repeat Steps 1-7 for each Θj in Θ
+            iterator.iternext()
+        nablaArray.append(gradient)
+    return tuple(nablaArray)
 
-
-def softmax_loss(X, y):
+# This function calculates the softmax loss.
+def calculateLossSoftMax(inputData, outputData):
     # Forward pass
-    N = X.shape[0]
-    exp_vals = np.exp(X)
+    N = inputData.shape[0]
+    exp_vals = np.exp(inputData)
     probs = exp_vals / np.sum(exp_vals, axis=1, keepdims=True)
-    loss = -np.mean(np.log(probs[range(N), y]))
+    loss = -np.mean(np.log(probs[range(N), outputData]))
     # Backward pass
     dX = np.array(probs, copy=True)
-    dX[range(N), y] -= 1
+    dX[range(N), outputData] -= 1
     dX /= N
     return loss, probs, dX
 
-
-def computeGrad(X, y, theta, reg):  # returns nabla
-    # WRITEME: write your code here to complete the routine
+# This function computes the gradient descent.
+def calculateGradient(inputData, outputData, theta, regularizationNum):  # returns nabla
     W, b = theta[0], theta[1]
-    f = X.dot(W) + b
-    _, _, df = softmax_loss(f, y)
-    dW = np.dot(X.T, df) + reg * W
+    f = inputData.dot(W) + b
+    _, _, df = calculateLossSoftMax(f, outputData)
+    dW = np.dot(inputData.T, df) + regularizationNum * W
     db = np.sum(df, axis=0)
     return (dW, db)
 
-
-def computeCost(X, y, theta, reg):
-    # WRITEME: write your code here to complete the routine
+# This function is the cost function.
+def calculateCost(inputData, outputData, theta, regularizationNum):
     W, b = theta[0], theta[1]
-    N = X.shape[0]
-    f = X.dot(W) + b
-    data_loss, _, _ = softmax_loss(f, y)
-    reg_loss = 0.5 * reg * np.sum(W ** 2)
+    N = inputData.shape[0]
+    f = inputData.dot(W) + b
+    data_loss, _, _ = calculateLossSoftMax(f, outputData)
+    reg_loss = 0.5 * regularizationNum * np.sum(W ** 2)
     cost = data_loss + reg_loss
     return cost
 
-
-def predict(X, theta):
-    # WRITEME: write your code here to complete the routine
+# This returns the classification for XOR.
+def classifyXOR(inputData, theta):
     W, b = theta[0], theta[1]
     # evaluate class scores
-    scores = X.dot(W) + b
-    _, probs, _ = softmax_loss(scores, y)
+    scores = inputData.dot(W) + b
+    _, probs, _ = calculateLossSoftMax(scores, y)
     return scores, probs
 
 
-np.random.seed(0)
-# Load in the data from disk
-path = os.getcwd() + '\\xor.dat'
-data = pd.read_csv(path, header=None)
+# Note this requires your XOR.dat to be in the same directory as the q1.py or this won't run.
+# I've also included this in my Readme.txt file.
+fileLocation = os.getcwd() + '\\xor.dat'
+file = pd.read_csv(fileLocation, header=None)
 
 # set X (training data) and y (target variable)
-cols = data.shape[1]
-X = data.iloc[:, 0:cols - 1]
-y = data.iloc[:, cols - 1:cols]
+column = file.shape[1]
+X = file.iloc[:, 0:column - 1]
+y = file.iloc[:, column - 1:column]
 
 # convert from data frames to numpy matrices
 X = np.array(X.values)
@@ -117,50 +108,49 @@ W = 0.01 * np.random.randn(D, K)
 b = np.zeros((1, K)) + 1.0
 theta = (W, b)
 
-# some hyperparameters
-reg = 1e-3  # regularization strength
-nabla_n = gradientUpdateRule(X, y, theta, reg)
-nabla = computeGrad(X, y, theta, reg)
-nabla_n = list(nabla_n)
+# Hyperparameters used
+regularization = 1e-3  # regularization strength
+nablaList = numericalGradientAlgorithm(X, y, theta, regularization)
+nabla = calculateGradient(X, y, theta, regularization)
+nablaList = list(nablaList)
 nabla = list(nabla)
 
-for jj in range(0, len(nabla)):
+for index in range(0, len(nabla)):
     is_incorrect = 0  # set to false
-    grad = nabla[jj]
-    grad_n = nabla_n[jj]
-    err = np.linalg.norm(grad_n - grad) / (np.linalg.norm(grad_n + grad))
-    if (err > 1e-8):
-        print("Param {0} is WRONG, error = {1}".format(jj, err))
+    gradient = nabla[index]
+    gradientList = nablaList[index]
+    error = np.linalg.norm(gradientList - gradient) / (np.linalg.norm(gradientList + gradient))
+    if (error > 1e-8):
+        print("Param {0} is WRONG, error = {1}".format(index, error))
     else:
-        print("Param {0} is CORRECT, error = {1}".format(jj, err))
+        print("Param {0} is CORRECT, error = {1}".format(index, error))
 
 # Re-initialize parameters for generic training
 W = 0.01 * np.random.randn(D, K)
 b = np.zeros((1, K))
 theta = (W, b)
 
-no_epochs = 1000
+numberOfEpochs = 1000
 check = 100  # every so many pass/epochs, print loss/error to terminal
-step_size = 0.1
-reg = 0.1  # regularization strength
+stepSize = 0.1
+regularization = 0.1  # regularization strength
 
 # gradient descent loop
 num_examples = X.shape[0]
-for i in range(no_epochs):
-    # WRITEME: write your code here to perform a step of gradient descent & record anything else desired for later
+for i in range(numberOfEpochs):
+    # Here I perform one step of gradient descent.
     theta = (W, b)
-    loss = computeCost(X, y, theta, reg)
+    loss = calculateCost(X, y, theta, regularization)
     if i % check == 0:
         print("Iteration [%d] - Loss [%f]" % (i, loss))
 
-    # perform a parameter update
-    # WRITEME: write your update rule(s) here
-    dW, db = computeGrad(X, y, theta, reg)
-    W = W - step_size * dW
-    b = b - step_size * db
+    # Here I update the parameter.
+    dW, db = calculateGradient(X, y, theta, regularization)
+    W = W - stepSize * dW
+    b = b - stepSize * db
 
 
-# evaluate training set accuracy
-scores, probs = predict(X, theta)
-predicted_class = np.argmax(scores, axis=1)
-print('Training Accuracy: %.2f%%' % (100 * np.mean(predicted_class == y)))
+# Here I see what the training set accuracy is.
+scores, probability = classifyXOR(X, theta)
+classPrediction = np.argmax(scores, axis=1)
+print('Training Accuracy: %.2f%%' % (100 * np.mean(classPrediction == y)))
